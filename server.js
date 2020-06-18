cabfd4efaef1523973973c22ac9b1dbe84999b79
Adam Goss <adam.goss94@gmail.com>
2020-06-18 13:55:03 +0200

    Add base endpoints

--- server.js
+++ server.js
@@ -1,7 +1,67 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = [
+    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+    { id: 3, author: 'Adam Goss', text: 'This company is worth every coin!' },
+    { id: 4, author: 'Jan Kowalski', text: 'They really know how to make you happy.' },
+  ];
+
+app.get('/testimonials', (req, res) => {
+    res.json(db);
+});
+
+app.get('/testimonials/random', (req, res) => {
+    const rand = Math.floor(Math.random()*db.length);
+    res.json(db[rand]);
+});
+
+app.get('/testimonials/:id', (req, res) => {
+    res.json(db.filter(item => item.id == req.params.id));
+});
+
+app.post('/testimonials', (req, res) => { 
+    const { author, text } = req.body
+    const payload = {
+        id: uuidv4(),
+        author: author,
+        text: text, 
+    };
+    db.push(payload);
+    res.json({ message: 'Record added' });
+});
+
+app.delete('/testimonials/:id', (req, res) => {
+
+    const position = db.findIndex(item => item.id == req.params.id)
+    db.splice(position, 1);
+
+    res.json({ message: 'Record deleted' });
+});
+
+app.put('/testimonials/:id', (req, res) => {
+    const { author, text } = req.body;  
+    const payload = {
+      id: req.params.id, 
+      author: author, 
+      text: text
+    }
+  
+    const position = db.findIndex(item => item.id == req.params.id);
+    db[position] = payload;  
+    res.json({ message: 'Record changed' });
+});
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Not found...' });
+});
+  
 app.listen(8000, () =>{
   console.log('Server is running on port: 8000');
-}); 
+})
