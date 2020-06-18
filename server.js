11846bf0b50d05d67b4c508a4c7fd150dc7848ae
Adam Goss <adam.goss94@gmail.com>
2020-06-18 14:12:17 +0200

    Move data to db.js and add new records

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const db = require('./db');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const db = [
+/* const db = [
     { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
     { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
     { id: 3, author: 'Adam Goss', text: 'This company is worth every coin!' },
     { id: 4, author: 'Jan Kowalski', text: 'They really know how to make you happy.' },
-  ];
+  ]; */
 
 app.get('/testimonials', (req, res) => {
-    res.json(db);
+    res.json(db.testimonials);
 });
 
 app.get('/testimonials/random', (req, res) => {
     const rand = Math.floor(Math.random()*db.length);
-    res.json(db[rand]);
+    res.json(db.testimonials[rand]);
 });
 
 app.get('/testimonials/:id', (req, res) => {
-    res.json(db.filter(item => item.id == req.params.id));
+    res.json(db.testimonials.filter(item => item.id == req.params.id));
 });
 
 app.post('/testimonials', (req, res) => { 
@@ -33,14 +34,14 @@ app.post('/testimonials', (req, res) => {
         author: author,
         text: text, 
     };
-    db.push(payload);
+    db.testimonials.push(payload);
     res.json({ message: 'Record added' });
 });
 
 app.delete('/testimonials/:id', (req, res) => {
 
-    const position = db.findIndex(item => item.id == req.params.id)
-    db.splice(position, 1);
+    const position = db.testimonials.findIndex(item => item.id == req.params.id)
+    db.testimonials.splice(position, 1);
 
     res.json({ message: 'Record deleted' });
 });
@@ -53,8 +54,8 @@ app.put('/testimonials/:id', (req, res) => {
       text: text
     }
   
-    const position = db.findIndex(item => item.id == req.params.id);
-    db[position] = payload;  
+    const position = db.testimonials.findIndex(item => item.id == req.params.id);
+    db.testimonials[position] = payload;  
     res.json({ message: 'Record changed' });
 });
 
