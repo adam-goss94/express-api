1f2988f6e81b755eb96b2437f3e163b87b9b2062
Adam Goss <adam.goss94@gmail.com>
2020-06-27 16:40:45 +0200

    Add functionality resonible for loadSeats from server with websocket

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require('express');
+const socket = require('socket.io');
+const app = express();
 const cors = require('cors');
 const path = require('path');
-
-const app = express();
-
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const testimonialsRoutes = require('./routes/testimionials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
@@ -22,9 +29,15 @@ app.get('*', (req, res) => {
 });
 
 app.use((req, res) => {
-    res.status(404).json({ message: 'Not found...' });
-});
-  
-app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
+  res.status(404).json({ message: 'Not found...' });
 })
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running...');
+});
+
+const io = socket(server);
+
+io.on('connection', () => {
+  console.log('New socket!')
+});
